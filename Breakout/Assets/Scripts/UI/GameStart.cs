98ca29c1da94179c7088c3be4325c52f8c105080
本村煌雅/Motokin <human.kagoshima.kouga@gmail.com>
2025-11-17 12:18:01 +0900

    提出用保存

---
+++ Breakout/Assets/Scripts/UI/GameStart.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class GameStartUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button _TargetButton;
+    [SerializeField]
+    private GameObject _TargetObject;
+    [SerializeField]
+    private Text _TargetText;
+    [SerializeField]
+    private Vector3 _StartPos;
+    [SerializeField]
+    private Vector3 _MovePos;
+    
+
+    void Start()
+    {
+        _TargetObject.transform.localPosition = _StartPos;
+        _TargetButton.onClick.AddListener(OnGameStartAnimation);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnGameStartAnimation()
+    {
+        _TargetObject.transform.DOLocalMove(_MovePos,1f).OnComplete(() =>
+        {
+            _TargetText.DOCounter(3,0,Time.deltaTime,true);
+        });
+    }
+}
